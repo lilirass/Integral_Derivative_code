4f478aac4b8587851b6920091a344c446a2450ff
Lili Rassouli <rassouli.leili@gmail.com>
2021-03-01 12:04:33 -0500

    add comments

--- auxiliary_differentiation.py
+++ auxiliary_differentiation.py
@@ -1,9 +1,9 @@
-import integrand_functions
+import Func_Analytical_Integral_Derivative
 
-def differentiate (a, func, func_params, h=0.0001):
+def differentiate (a, func, func_params, h=0.1):
     """
  this function will calculate the value of derivative function in one point
- central difference formula: f'(a) = [f(a+h) - f(a-h)]/(2h)
+ central difference formula for numerical differentiation: f'(a) = [f(a+h) - f(a-h)]/(2h)
  func = function 
  a = compute the derivative at this point 
  func_params = a dictionary of all function's parameters
@@ -13,5 +13,6 @@ def differentiate (a, func, func_params, h=0.0001):
     return derivative
 
 
-print (differentiate (2, integrand_functions.general_power_func, {"x_multiple":3, "x_shift":3.0 ,"power":2, "y_shift":3 }))
-print (integrand_functions.derivative_general_power_func (2, {"x_multiple":3, "x_shift":3.0 ,"power":2 }))
+#to check derivatives
+#print (differentiate (2, Func_Analytical_Integral_Derivative.general_power_func, {"x_multiple":3, "x_shift":3.0 ,"power":2, "y_shift":3 }))
+#print (Func_Analytical_Integral_Derivative.derivative_general_power_func (2, {"x_multiple":3, "x_shift":3.0 ,"power":2 }))
